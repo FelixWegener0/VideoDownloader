f610c477ab863f54fd8a81e74f1841b31813ed5a
Felix Wegener <Felix.Wegener05@t-online.de>
2024-01-08 15:24:39 +0100

    add support to directly post video url in pipe

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from pytube import YouTube
 from moviepy.editor import AudioFileClip
 import os
+import sys
+
 
 def downloadToPath(url):
     path = './videos'
     video = YouTube(url)
-    video = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    video = video.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first()
     if not os.path.exists(path):
         os.makedirs(path)
     video.download(path)
     print('downloaded')
 
+
 def mp4ToMp3():
     print('changing format to mp3')
     mp4 = os.listdir('./videos')
-    mp3 = mp4[0].replace('mp4','mp3')
+    mp3 = mp4[0].replace('mp4', 'mp3')
     convertFile = AudioFileClip('./videos/' + mp4[0])
     convertFile.write_audiofile('./videos/' + mp3)
     convertFile.close()
     os.remove('./videos/' + mp4[0])
 
+
 if (__name__ == '__main__'):
-    url = input('insert the youtube video Url:')
+    if (not sys.argv[1]):
+        url = input('insert the youtube video Url:')
+    else:
+        url = sys.argv[1]
     downloadToPath(url)
     mp4ToMp3()
-    
